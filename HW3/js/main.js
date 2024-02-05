8676dafe58c5ac4d79de901ffdb79acfeefaa852
Muzvitaliy <126752329+Muzvitaliy@users.noreply.github.com>
2024-02-05 20:18:35 +0100

    Add files via upload

---
+++ HW3/js/main.js
@@ -0,0 +1,73 @@
+"use strict"
+
+// Вивід парних чисел
+
+// let userNumber = prompt("Напишіть ваше число: ");
+
+// if (isNaN(userNumber)) {
+//    console.log("Помилка, введіть, будь ласка число: ");
+// }
+// else {
+//    for (let i = 2; i < userNumber; i += 2) {
+//       console.log(i)
+//    }
+// }
+
+// while
+
+// if (isNaN(userNumber)) {
+//    console.log("Помилка, введіть, будь ласка число: ");
+// }
+// else {
+//    let i = 2;
+//    while (i < userNumber) {
+//       console.log(i)
+//       i += 2;
+//    }
+// }
+
+// FizzBuzz
+
+// for (let i = 1; i <= 100; i++) {
+//    if (i % 15 === 0) {
+//       console.log("FizzBuzz");
+//    } else if (i % 3 == 0) {
+//       console.log("Fizz");
+//    } else if (i % 5 == 0) {
+//       console.log("Buzz");
+//    } else {
+//       console.log(i);
+//    }
+// }
+
+// Додаткова задача *
+
+let string = '42559125';
+
+// let result = '';
+
+// for (let i = 0; i <= string.length;)
+
+//    let i = 0;
+
+// while (result.length <= string.length) {
+//    if (string[1] < 5) {
+//       // result + "0";
+//       console.log(result + "0")
+//    } else (string[1] >= 5) {
+//       // result + "1";
+//       console.log(result + "1")
+//    }
+//    console.log(result);
+// }
+
+// let i;
+
+// for (let result; result.length <= string.length; result + i) {
+//    if (string[1] < 5) {
+//       i == 0;
+//    } else (string[1] >= 5) {
+//       i == 1;
+//    }
+//    console.log(result);
+// }
